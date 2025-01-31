a62f1f8416a682311031e0703bc08469aaad6c10
josehtat <jhurtadot33@gmail.com>
2025-01-31 16:59:09 +0100

    agregacion de base_site.html

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 import datetime
+
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,9 +9,13 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
